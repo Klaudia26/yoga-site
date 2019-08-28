aa8c7dc64655ce5479032cbdf64f031ceffb856c
Klaudia <klaudia.krzywda@gmail.com>
2019-08-28 15:55:36 +0100

    add section about

---
+++ src/components/common/Fitness/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+const Fitness = () => {
+    return (
+        <div className="fitness">
+            <div className="fitness__content">
+                <h3 className="fitness__title">Get your fitness back today</h3>
+                <p className="fitness__subtitle">It is a long established fact that a reader will be distracted</p>
+            </div>
+            <button className="btn">Get your free registration</button>
+        </div>
+    );
+}
+
+export default Fitness;
--- src/components/common/Heading/index.js
+++ src/components/common/Heading/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.scss';
 
-const Heading = () => {
+const Heading = ({ num, part1, part2 }) => {
     return (
-        <h1>
-            heading
-        </h1>
-    )
+        <h2 className="heading-secondary">
+            <span className="heading-secondary__number">{num}</span>
+            <span className="heading-secondary__part-one"> {part1}</span>
+            <span className="heading-secondary__part-two"> {part2}</span>
+        </h2>
+    );
 }
 
 export default Heading;
